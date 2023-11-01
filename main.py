0302ae5d8b77810b1e33dfd25651d978f2a933ef
Your Name <arturakopan099@gmail.com.com>
2023-11-01 12:37:49 +0800

    interface

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
-def Simple_num(num):
-    for i in range(2, num):
-        if num%i==0:
-            return False
-    return True
-
-def Palindrom(str):
-    str = str.lower().replace(" ", "")
-    for i in range(len(str)//2):
-        if str[i]!=str[(i * -1) - 1]:
-            return False
-    return True
-
-str = input()
-print(Palindrom(str))
+while True:
+    answer = input("Действия с базой данных:\n1.Добавить значение\n2.Удалить значение\n3.Просмотреть базу\n")
+    while True:
+        if 1<=int(answer)<=3:
+            break
+        answer = input()
+    if int(answer)==1:
+        pass
+    elif int(answer)==2:
+        pass
+    else:
+        pass
